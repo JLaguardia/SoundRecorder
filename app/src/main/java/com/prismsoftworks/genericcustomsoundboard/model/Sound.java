03695297d6fc78263fd6bd35078f195f8b4b7b2a
Anthony Todd <awtodd89@gmail.com>
2017-08-04 17:05:20 -0400

    Recyclerview, Gradle 4, Android Studio 3.0
    Added a recyclerview for easy manipulation of data. Removed the dialog
    since it was no longer being used. Added custom icons from the google
    open source website (material.io/icons). Touched up the MediaPlayer with
    a custom media player so that files do not try to out play on another.
    This is also responsible for handling the image view icon while it's
    playing.
    
    Removed unnecessary layout files.
    
    @todo handle the edit button to change file names. This will be handled
    by the adapter with some utility calls

--- app/src/main/java/com/prismsoftworks/genericcustomsoundboard/model/Sound.java
+++ app/src/main/java/com/prismsoftworks/genericcustomsoundboard/model/Sound.java
@@ -1,38 +1,38 @@
-package com.prismsoftworks.genericcustomsoundboard.object;
+package com.prismsoftworks.genericcustomsoundboard.model;
 
 import java.io.File;
 
 /**
  * Created by james/CarbonDawg on 8/18/16.
- *
  */
-public class SoundObject {
+public class Sound {
     private String mTitle;
     private File mSoundFile;
     private boolean editMode = false;
 
-    public SoundObject(String title, File file){
+    public Sound(String title, File file) {
         mTitle = title;
         mSoundFile = file;
     }
 
-    public void setTitle(String title){//probably dont need any mutator methods
+    public void setTitle(String title) {//probably dont need any mutator methods
         mTitle = title;
     }
 
-    public String getTitle(){
+    public String getTitle() {
         return mTitle;
     }
 
-    public File getSoundFile(){
+    public File getSoundFile() {
         return mSoundFile;
     }
 
-    public boolean isEditMode(){
+    public boolean isEditMode() {
         return editMode;
     }
 
-    public void toggleEditMode(){
+    public void toggleEditMode() {
         editMode = !editMode;
     }
 }
+
